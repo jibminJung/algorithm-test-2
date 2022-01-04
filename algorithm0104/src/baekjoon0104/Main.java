29b6a710cef2dfb45956ceb4c0933bfd2f6b1299
jibminJung <jibminjung@gmail.com>
2022-01-04 23:18:32 +0900

    Dynamic Programming

--- algorithm0104/src/baekjoon0104/Main.java
+++ algorithm0104/src/baekjoon0104/Main.java
@@ -1,4 +1,4 @@
-package baekjoon1520;
+package baekjoon0104;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,7 +7,5 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-
     }
 }
