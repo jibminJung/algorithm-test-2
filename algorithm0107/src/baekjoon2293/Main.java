5501226a72ec919f96046ffc6d4dbd39a1586375
jibminJung <jibminjung@gmail.com>
2022-01-07 08:18:56 +0900

    DP

---
+++ algorithm0107/src/baekjoon2293/Main.java
@@ -0,0 +1,22 @@
+package baekjoon2293;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n,k;
+    static int[] coin;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        coin = new int[n];
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+    }
+}
