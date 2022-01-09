c693764dd73d512b35361f7082f15e85ed1be33e
jibminJung <jibminjung@gmail.com>
2022-01-09 21:24:12 +0900

    Hacker Rank

---
+++ algorithm0109/src/hackerranklargestpermutation/Solution.java
@@ -0,0 +1,76 @@
+package hackerranklargestpermutation;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'largestPermutation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> largestPermutation(int k, List<Integer> arr) {
+        int[] idx = new int[arr.size()+1];
+        for (int i = 0; i < arr.size(); i++) {
+            idx[arr.get(i)] = i;
+        }
+
+        for (int i = 0,n=arr.size(); i < k && n>0; i++,n--) {
+            int temp = arr.get(i);
+            if(temp==n){
+                k++;
+                continue;
+            }
+            int index = idx[n];
+            idx[temp] = index;
+            arr.set(i,n);
+            arr.set(index,temp);
+        }
+        return arr;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.largestPermutation(k, arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
