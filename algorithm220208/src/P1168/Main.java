de083ec813050c2bfa9fa7a396fd2967498d9bff
jibminJung <jibminjung@gmail.com>
2022-02-09 00:42:06 +0900

    Segment Tree

--- algorithm220208/src/P1168/Main.java
+++ algorithm220208/src/P1168/Main.java
@@ -7,27 +7,60 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] tree;
+    static int n,s=1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken())-1;
-        int index = 0;
-        ArrayList<Integer> arr= new ArrayList<>();
+        n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        getS();
+        tree = new int[s*2];
+        int index = 1;
         for (int i = 1; i < n + 1; i++) {
-            arr.add(i);
+            update(i,true);
         }
         sb.append('<');
         for (int i = 0; i < n-1; i++) {
-            index += k;
-            if(index>=arr.size()){
-                index %= arr.size();
+            index += k-1;
+            if(index%tree[1]==0){
+                index = tree[1] ;
+            }else if (index > tree[1]){
+                index %= tree[1];
             }
-            sb.append(arr.remove(index)).append(", ");
+            int temp = query(1,index);
+            sb.append(temp).append(", ");
+            update(temp,false);
         }
-        sb.append(arr.remove(0)).append('>');
+        sb.append(query(1,1)).append('>');
         System.out.println(sb);
 
+    }
+
+    static void getS() {
+        while (s < n) {
+            s <<= 1;
+        }
+    }
+
+    static void update(int number, boolean isInput) {
+        int index = s + number-1;
+        int diff = isInput ? 1 : -1;
+        while (index > 0) {
+            tree[index] += diff;
+            index /= 2;
+        }
+    }
+    static int query(int node,int position){
+        if(node>=s){
+            return node-s+1;
+        }
+        int left = tree[node*2];
+        if(position<=left){
+            return query(node*2,position);
+        }else{
+            return query(node*2+1,position-left);
+        }
     }
 }
